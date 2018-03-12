12a4db57a4f8cb5f7bcf3ca1758c1e9bd388e6c9
liuguangxin2 <liuguangxin@jd.com>
2018-03-12 21:19:43 +0800

    add info

---
+++ src/main/java/com/daxin/happy/learn/note/HappyLearnEveryDay.java
@@ -0,0 +1,13 @@
+package com.daxin.happy.learn.note;
+
+/**
+ * Hello world!
+ *
+ */
+public class HappyLearnEveryDay {
+	public static void main(String[] args) {
+		System.out.println("======================================");
+		System.out.println("==     好的习惯需要养成,一旦养成就要坚持!     ==");
+		System.out.println("======================================");
+	}
+}
